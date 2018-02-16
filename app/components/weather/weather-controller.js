32a2f3307ce8c02a8d6dab2aad5332efbdfc5a58
Benjamen King <brk.3rdof3kings@gmail.com>
2018-02-16 10:24:47 -0700

    Complete basic implementation of weather component

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -1,10 +1,29 @@
 function WeatherController(){
-	var wc = this;
+	
 	var weatherService = new WeatherService();
+
+	var $parentDiv = $('div#weather')
+	var weatherTmp = `
+		<img class="weather-icon" src="" alt="weather icon">
+		<span class="weather-temp"></span>
+		<span class="weather-loc"></span>
+	`
 	
 	weatherService.getWeather(function(weather){
-		console.log(weather);
-		//What can you do with this weather object?
+		// console.log(weather);
+		var tempK = weather.main.temp
+		var tempF = Math.floor((tempK - 273.15) * 1.8 + 32)
+		var loc = weather.name
+		var iconCode = weather.weather[0].icon
+		var iconSrc = `http://openweathermap.org/img/w/${iconCode}.png`
+		drawWeather(iconSrc, tempF, loc)
 	})
+
+	function drawWeather(iconSrc, tempF, loc) {
+		$parentDiv.html(weatherTmp)
+		$('img.weather-icon').attr("src", iconSrc)
+		$('span.weather-temp').text(tempF + "deg F")
+		$('span.weather-loc').text(loc)
+	}
 
 }
