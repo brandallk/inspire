2bf2d158e0c605464bd9d58b1bd6e8ac5a184510
Benjamen King <brk.3rdof3kings@gmail.com>
2018-02-17 21:18:50 -0700

    Complete initial implementation of settings feature

--- app/components/settings/settings-controller.js
+++ app/components/settings/settings-controller.js
@@ -2,24 +2,85 @@ function SettingsController() {
 
     var settingsService = new SettingsService()
     var $parentDiv = $('#settings')
+    var formTmp = `
+        <form class="edit-settings">
+            <f3 class="text-center">Settings</f3>
+			<div class="form-group">
+				<label for="set-user" class="">Username:</label>
+				<input id="set-user" class="form-control" type="text" placeholder="Your name" name="user">
+			</div>
+			<div class="form-group">
+				<label for="set-timeformat" class="">Time format:</label>
+                <select id="set-timeformat" class="form-control" name="timeformat">
+                    <option>12-hour</option>
+                    <option>24-hour</option>
+                </select>
+			</div>
+			<div class="form-group">
+				<label for="set-theme" class="">Theme:</label>
+                <select id="set-theme" class="form-control" name="theme">
+                    <option>dark</option>
+                    <option>light</option>
+                </select>
+			</div>
+			<div class="form-group">
+				<label for="set-tempscale" class="">Temp Scale:</label>
+                <select id="set-tempscale" class="form-control" name="tempscale">
+                    <option value="F">Fahrenheit</option>
+                    <option value="C">Celcius</option>
+                    <option value="K">Kelvin</option>
+                </select>
+			</div>
+			<button type="submit" class="submit btn btn-sm btn-light btn-block">Submit</button>
+		</form>
+    `
 
-    function drawToggleIcon() {
-        $parentDiv.html(`<i class="settings-toggle fas fa-cog"></i>`)
+    function drawToggleIcon() { console.log('drawing toggle icon')
+        $parentDiv.html(`<a href="#" class="settings-toggle"><i class="fas fa-cog"></i></a>`)
         listenForToggle()
     }
 
-    function listenForToggle() {
-        // event listener for click on the toggle icon
-        var $toggleBtn = $('.settings-toggle')
-        $toggleBtn.on('click', (evt) => {
+    function listenForToggle() { console.log('listening for toggle')
+        var $toggleBtn = $('.settings-toggle');  console.log('toggle icon:', $toggleBtn)
+        $toggleBtn.on('click', (evt) => { console.log('click on toggle icon')
+            evt.preventDefault()
             settingsService.getSettings(drawSettingsForm)
         })
     }
 
     function drawSettingsForm(settings) {
-        // use the current settings, build the form, add it to the DOM, and attach listeners for the controls
-
+        $parentDiv.html(formTmp)
+        $('#set-user').get(0).value = settings.user
+        $('#set-timeformat').get(0).value = settings.timeFormat
+        $('#set-theme').get(0).value = settings.theme
+        $('#set-tempscale').get(0).value = settings.tempScale
+        listenForSettingChange()
     }
 
-    drawToggleIcon()
+    function listenForSettingChange() {
+        var $form = $('.edit-settings')
+        $form.on('submit', (evt) => {
+            evt.preventDefault()
+            var settings = {
+                user: $form.get(0).user.value,
+                timeFormat: $form.get(0).timeformat.value,
+                theme: $form.get(0).theme.value,
+                tempScale: $form.get(0).tempscale.value
+            }
+            settingsService.setSettings(settings, drawToggleIcon)
+        })
+    }
+
+    this.getSettings = function() {
+        settingsService.getSettings( (res) => {
+            // if a settings object already exists on the server...
+            if (res) {
+                return res // ... return it
+            } else {
+                return settingsService.getDefaultSettings() // ...otherwise return defaults
+            }
+        })
+    }
+
+    settingsService.setSettings(settingsService.getDefaultSettings(), drawToggleIcon)
 }
--- app/components/settings/settings-service.js
+++ app/components/settings/settings-service.js
@@ -1,64 +1,79 @@
 function SettingsService() {
 
     function Settings(settings) {
-        this.user = settings.user,
-        this.timeFormat = settings.timeFormat,
-        this.theme = settings.themeObj,
-        this.tempScale = settings.tempScale
+        for (let key in settings) {
+            this[key] = settings[key]
+        }
+        // this.user = settings.user,
+        // this.timeFormat = settings.timeFormat,
+        // this.theme = settings.theme,
+        // this.tempScale = settings.tempScale
     }
 
+    var currentSettings = {}
     var defaultSettings = {
         user: null,
         timeFormat: "12h",
-        theme: {
-            font: "sans-serif",
-            fontColor: "rgb(255,255,255)"
-        },
+        theme: "dark",
         tempScale: "F"
     }
 
-    var settingsCache = null
-    var baseUrl = 'https://inspire-server.herokuapp.com/api/brk83604/settings'
+    var baseUrl = 'https://inspire-server.herokuapp.com/api/brk83604settings'
     
     function logError(err) {
 		console.error('UMM SOMETHING BROKE: ', err)
-	}
+    }
+    
+    this.getDefaultSettings = function() {
+        return defaultSettings
+    }
     
     this.getSettings = function(cb) {
         $.get(baseUrl)
         .then( res => {
-            console.log(res)
-            // settingsCache = res
-            // cb(res)
+            // console.log('got settings:', res[0])
+            currentSettings = res[0]
+            cb(res[0])
         })
         .fail(logError)
     }
 
-    this.addSettings = function(settings, cb) {
+    this.setSettings = function(settings, cb) {
+        this.getSettings( (res) => {
+            // if a settings object already exists on the server...
+            if (res) {
+                // console.log('found existing settings:', res)
+                this.updateSettings(settings, cb) // ...update it
+            } else {
+                // console.log('found NO existing settings; creating new')
+                this.createSettings(settings, cb) // ...otherwise, create it
+            }
+        })
+    }
+
+    this.createSettings = function(settings, cb) {
         var newSettings = new Settings(settings)
         $.post(baseUrl, newSettings)
         .then( res => {
-            console.log(res)
-            // settingsCache = res.data
-            // cb(res.data)
-        })
-        .fail(logError)
-    }
-
-    this.removeSetting = function(settingName, setting, cb) {
-        var settings = settingsCache
-        settings[settingName] = setting
-        var id = settings.id // settingsCache should have an id from the server response
-        $.ajax({
-            method: 'PUT',
-            contentType: 'application/json',
-            url: baseUrl + '/' + id,
-            data: JSON.stringify(settings)
-        })
-        .then( res => {
+            // console.log('new settings:', res.data)
             this.getSettings(cb)
         })
         .fail(logError)
     }
 
+    this.updateSettings = function(settings, cb) {
+        var newSettings = new Settings(settings)
+        var settingsID = currentSettings.id
+        $.ajax({
+			method: 'PUT',
+			contentType: 'application/json',
+			url: baseUrl + '/' + settingsID,
+			data: JSON.stringify(newSettings)
+        })
+        .then( (res) => {
+            // console.log('updated settings:', res)
+            this.getSettings(cb)
+        })
+        .fail(logError)
+    }
 }
