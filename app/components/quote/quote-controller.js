0cb9d1c49a94788d52468c4f4dfbe1c3a5116bfd
Benjamen King <brk.3rdof3kings@gmail.com>
2018-02-19 06:19:55 -0700

    Rebuild quote-controller.js using Vue

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -2,22 +2,65 @@ function QuoteController(){
 
 	var quoteService = new QuoteService()
 
-	var $parentDiv = $('#quote')
-	var quoteTmp = `
-		<p class="quote-text"></p>
-		<span class="quote-author"></span>
-	`
+	let Quote = {
+		template: `
+			<div>
+				<p class="quote-text text-center" v-on:mouseenter="mouseenter" v-on:mouseleave="mouseleave" v-html="quoteText"></p>
+				<span class="quote-author" v-if="showAuthor" v-html="quoteAuthor"></span>
+			</div>
+		`,
+		data() {
+			return {
+				quoteText: '',
+				quoteAuthor: '',
+				showAuthor: false
+			}
+		},
+		methods: {
+			mouseenter() {
+				setTimeout( () => {
+					this.showAuthor = true
+				}, 500)
+			},
+			mouseleave() {
+				setTimeout( () => {
+					this.showAuthor = false
+				}, 1000)
+			}
+		},
+		mounted() {
+			quoteService.getQuote( res => {
+				this.quoteText = res.quote
+				this.quoteAuthor = "-- " + res.author
+			})
+		}
+	}
 
-	quoteService.getQuote(function(quote){
-		// console.log('What is the quote', quote)
-		var text = quote.quote
-		var author = quote.author
-		drawQuote(text, author)
+	let vue = new Vue({
+		el: '#quote',
+		components: {
+			'quote': Quote
+		}
 	})
 
-	function drawQuote(text, author) {
-		$parentDiv.html(quoteTmp)
-		$('.quote-text').html(`"${text}"`)
-		$('.quote-author').text(`-- ${author}`)
-	}
+	// Original implementation (not using Vue):
+
+	// var $parentDiv = $('#quote')
+	// var quoteTmp = `
+	// 	<p class="quote-text"></p>
+	// 	<span class="quote-author"></span>
+	// `
+
+	// quoteService.getQuote(function(quote){
+	// 	// console.log('What is the quote', quote)
+	// 	var text = quote.quote
+	// 	var author = quote.author
+	// 	drawQuote(text, author)
+	// })
+
+	// function drawQuote(text, author) {
+	// 	$parentDiv.html(quoteTmp)
+	// 	$('.quote-text').html(`"${text}"`)
+	// 	$('.quote-author').text(`-- ${author}`)
+	// }
 }
